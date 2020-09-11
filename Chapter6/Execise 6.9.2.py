2512820a33bf61f74e34d690525591166c35510b
Vamger <mirza.adam@hotmail.com>
2020-09-11 13:11:54 +0200

    Added changes

---
+++ Chapter6/Execise 6.9.2.py
@@ -0,0 +1,8 @@
+# Write a function day_name that converts an integer number o to 6
+# into the name of the day. Assume day 0 is "Sunday". Once again,
+# return None if the arguments to the function are not valid.
+# Here are some tests that should pass:
+
+test(day_name(3) == "Wednesday")
+test(day_name(6) == "Saturday")
+test(day_name(42) == None )
