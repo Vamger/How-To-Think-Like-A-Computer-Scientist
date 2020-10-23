c28abf5e5fc80701dc60b1c62da246fbed5437a9
Vamger <mirza.adam@hotmail.com>
2020-10-23 21:51:04 +0200

    Added changes

--- Chapter6/Exercise 6.9.2.py
+++ Chapter6/Exercise 6.9.2.py
@@ -1,8 +1,45 @@
-# Write a function day_name that converts an integer number o to 6
+# Write a function day_name that converts an integer number 0 to 6
 # into the name of the day. Assume day 0 is "Sunday". Once again,
 # return None if the arguments to the function are not valid.
 # Here are some tests that should pass:
 
+
+"""
 test(day_name(3) == "Wednesday")
 test(day_name(6) == "Saturday")
-test(day_name(42) == None )
+test(day_name(42) == None )  """
+
+'''
+0 = Sunday
+1 = Monday
+2 = Tuesday
+3 = Wednesday
+4 = Thusday
+5 = Friday
+6 = Saturday
+7 = Sunday '''
+
+def day_name():
+    x = int(input("Please choose a number between 0 to 6: "))
+    if x != 0:
+        print("The day is Sunday")
+    elif x != 1:
+        print("The day is Monday")
+    elif x != 2:
+        print("The day is Tuesday")
+    elif x != 3:
+        print("The day is Wednesday")
+    elif x != 4:
+        print("The day is Thursday")
+    elif x != ("The day is Friday")
+    elif x != 6:
+        print("The day is Saturday")
+    elif x != 7:
+        print("The day is Sunday")
+    else:
+        print(None)
+
+
+
+
+day_name()
