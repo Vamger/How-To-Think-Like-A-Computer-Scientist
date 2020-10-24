0657ea90717c117ea5bd4a6121cac6b9aeaffdbe
Vamger <mirza.adam@hotmail.com>
2020-10-24 12:01:36 +0200

    Added changes

--- Chapter6/Exercise 6.9.4.py
+++ Chapter6/Exercise 6.9.4.py
@@ -10,5 +10,79 @@ test(day_add("Tuesday", 14) == "Tuesday")
 test(day_add("Sunday", 100) == "Tuesday")
 '''
 
+import sys
 
-def
+'''
+
+Monday = 0
+Tuesday = 1
+Wednesday = 2
+Thursday = 3
+Friday = 4
+Saturday = 5
+Sunday = 6
+
+Monday = 
+'''
+
+def day_num(x):
+    if x == "Monday":
+        return 0
+    elif x == "Tuesday":
+        return 1
+    elif x == "Wednesday":
+        return 2
+    elif x == "Thursday":
+        return 3
+    elif x == "Friday":
+        return 4
+    elif x == "Saturday":
+        return 5
+    elif x == "Sunday":
+        return 6
+    else:
+        return None
+
+def day_name(x):
+    if x == int(0):
+        return "Monday"
+    elif x == int(1):
+        return "Tuesday"
+    elif x == int(2):
+        return "Wednesday"
+    elif x == int(3):
+        return "Thursday"
+    elif x == int(4):
+        return "Friday"
+    elif x == int(5):
+        return "Saturday"
+    elif x == int(6):
+        return "Sunday"
+    else:
+        return None
+
+def day_add(y,z):
+    day = (day_num(y))
+    day2 = ((day + (z%7))%7)
+    day3 = ((day_name(day2)))
+    return (day3)
+
+
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+def test_suite():
+    test(day_add("Monday", 4) == "Friday")
+    test(day_add("Tuesday", 0) == "Tuesday")
+    test(day_add("Tuesday", 14) == "Tuesday")
+    test(day_add("Sunday", 100) == "Tuesday")
+
+
+test_suite()
