041b894e38860149d42e15e5c585822254f99523
Vamger <mirza.adam@hotmail.com>
2020-10-24 14:34:52 +0200

    Added changes

---
+++ Chapter6/Exercise 6.9.7.py
@@ -0,0 +1,41 @@
+# Write a function to_secs that converts hours,
+# minutes and seconds to a total number of seconds.
+# Here are some tests that should pass:
+
+'''
+test(to_secs(2, 30, 10) == 9010)
+test(to_secs(2, 0, 0) == 7200)
+test(to_secs(0, 2, 0) == 120)
+test(to_secs(0, 0, 42) == 42)
+test(to_secs(0, -10, 10) == -590)
+'''
+
+import sys
+
+def to_secs(x, y, z):
+    """z = seconds, y = minutes & x = hours"""
+    s = (z + (y*60)+(x*3600))
+    return s
+
+
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def test_suite():
+    """ Run the suite of tests for code in this module (this file).
+    """
+    test(to_secs(2, 30, 10) == 9010)
+    test(to_secs(2, 0, 0) == 7200)
+    test(to_secs(0, 2, 0) == 120)
+    test(to_secs(0, 0, 42) == 42)
+    test(to_secs(0, -10, 10) == -590)
+
+test_suite()
