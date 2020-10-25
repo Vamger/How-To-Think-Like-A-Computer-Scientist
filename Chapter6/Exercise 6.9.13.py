c54fbae92a52dba95c2a9e89db9a9cd38b448852
Vamger <mirza.adam@hotmail.com>
2020-10-25 19:09:15 +0100

    Added changes

---
+++ Chapter6/Exercise 6.9.13.py
@@ -0,0 +1,15 @@
+# Write a function slope(x1, y1, x2, y2)
+# that returns the slope of the line through
+# the points (x1, y1) and (x2, y2).
+# Be sure your implementation of slope can
+# pass the following tests:
+
+'''
+
+test(slope(5, 3, 4, 2) == 1.0)
+test(slope(1, 2, 3, 2) == 0.0)
+test(slope(1, 2, 3, 3) == 0.5)
+test(slope(2, 4, 1, 2) == 2.0)
+
+'''
+
