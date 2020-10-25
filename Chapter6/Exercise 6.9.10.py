04fe4b584369a6722c2680e2159b748f4b35bf9f
Vamger <mirza.adam@hotmail.com>
2020-10-25 15:10:13 +0100

    Added changes

--- Chapter6/Exercise 6.9.10.py
+++ Chapter6/Exercise 6.9.10.py
@@ -11,10 +11,10 @@ def test(did_pass):
     print(msg)
 
 
-test(3 % 4 == 0)
-test(3 % 4 == 3)
-test(3 / 4 == 0)
-test(3 // 4 == 0)
-test(3+4  *  2 == 14)
-test(4-2+2 == 0)
-test(len("hello, world!") == 13)
+test(3 % 4 == 0) #Fails because the remainder between 3 and 4 is NOT 0
+test(3 % 4 == 3) # Works because the remainder can only reach 3
+test(3 / 4 == 0) # Fails because 3 divided by 4 is exactly 0.75 and not 0
+test(3 // 4 == 0) # Works because the result is 0,something which is ok when using //
+test(3+4  *  2 == 14) # Fails because 8 + 3 is not 14
+test(4-2+2 == 0) # Fails because 4 + 2 - 2 is not 0
+test(len("hello, world!") == 13) # There is 13 letters (space and , aswell) in Hello, world!
