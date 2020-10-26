3fc5aff7c14b9a4d077ddefb681dd60c53108fc1
Vamger <mirza.adam@hotmail.com>
2020-10-26 23:21:42 +0100

    Added changes

--- Chapter6/Exercise 6.9.13.py
+++ Chapter6/Exercise 6.9.13.py
@@ -13,3 +13,29 @@ test(slope(2, 4, 1, 2) == 2.0)
 
 '''
 
+import sys
+
+def slope(x1, y1, x2, y2):
+    s = (y2 - y1) / (x2 - x1)
+    return s
+
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def test_suite():
+    """ Run the suite of tests for code in this module (this file).
+    """
+    test(slope(5, 3, 4, 2) == 1.0)
+    test(slope(1, 2, 3, 2) == 0.0)
+    test(slope(1, 2, 3, 3) == 0.5)
+    test(slope(2, 4, 1, 2) == 2.0)
+
+test_suite()
