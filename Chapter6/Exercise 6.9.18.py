3df7dbd41915c9fd50c60a4544be9d24091ea332
Vamger <mirza.adamu@hotmail.com>
2020-10-30 00:57:10 +0100

    Added changes

--- Chapter6/Exercise 6.9.18.py
+++ Chapter6/Exercise 6.9.18.py
@@ -1,27 +1,24 @@
-# Write the function f2c(t) designed to return the integer
-# value of the nearest degree Celsius for given temperature in Fahrenheit.
-# (hint: you may want to make use of the built-in function, round.
-# Try printing round.__doc__ in a Python shell or looking up help for the round function,
-# and experimenting with it until you are comfortable with how it works.)
+
+# Now do the opposite: write the function c2f which converts Celsius to Fahrenheit:
+
+'''
+test(c2f(0) == 32)
+test(c2f(100) == 212)
+test(c2f(-40) == -40)
+test(c2f(12) == 54)
+test(c2f(18) == 64)
+test(c2f(-48) == -54)
+
 
 '''
 
-test(f2c(212) == 100)     # Boiling point of water
-test(f2c(32) == 0)        # Freezing point of water
-test(f2c(-40) == -40)     # Wow, what an interesting case!
-test(f2c(36) == 2)
-test(f2c(37) == 3)
-test(f2c(38) == 3)
-test(f2c(39) == 4)
 
-'''
-
-# Formula: T(°C) = (T(°F) - 32) / 1.8
+# Formula: T(°F) = T(°C) × 1.8 + 32
 
 import sys
 
-def f2c(x):
-    y = round((x - 32)/1.8)
+def c2f(x):
+    y = round((x * 1.8) + 32)
     return y
 
 
@@ -38,13 +35,12 @@ def test(did_pass):
 def test_suite():
     """ Run the suite of tests for code in this module (this file).
     """
-    test(f2c(212) == 100)  # Boiling point of water
-    test(f2c(32) == 0)  # Freezing point of water
-    test(f2c(-40) == -40)  # Wow, what an interesting case!
-    test(f2c(36) == 2)
-    test(f2c(37) == 3)
-    test(f2c(38) == 3)
-    test(f2c(39) == 4)
+    test(c2f(0) == 32)
+    test(c2f(100) == 212)
+    test(c2f(-40) == -40)
+    test(c2f(12) == 54)
+    test(c2f(18) == 64)
+    test(c2f(-48) == -54)
 
 test_suite()
 
