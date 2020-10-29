dc314a63a60ef2094cf5e94342a3bc773d92b43b
Vamger <mirza.adamu@hotmail.com>
2020-10-30 00:51:22 +0100

    Added changes

---
+++ Chapter6/Exercise 6..9.18.py
@@ -0,0 +1,50 @@
+# Write the function f2c(t) designed to return the integer
+# value of the nearest degree Celsius for given temperature in Fahrenheit.
+# (hint: you may want to make use of the built-in function, round.
+# Try printing round.__doc__ in a Python shell or looking up help for the round function,
+# and experimenting with it until you are comfortable with how it works.)
+
+'''
+
+test(f2c(212) == 100)     # Boiling point of water
+test(f2c(32) == 0)        # Freezing point of water
+test(f2c(-40) == -40)     # Wow, what an interesting case!
+test(f2c(36) == 2)
+test(f2c(37) == 3)
+test(f2c(38) == 3)
+test(f2c(39) == 4)
+
+'''
+
+# Formula: T(°C) = (T(°F) - 32) / 1.8
+
+import sys
+
+def f2c(x):
+    y = round((x - 32)/1.8)
+    return y
+
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def test_suite():
+    """ Run the suite of tests for code in this module (this file).
+    """
+    test(f2c(212) == 100)  # Boiling point of water
+    test(f2c(32) == 0)  # Freezing point of water
+    test(f2c(-40) == -40)  # Wow, what an interesting case!
+    test(f2c(36) == 2)
+    test(f2c(37) == 3)
+    test(f2c(38) == 3)
+    test(f2c(39) == 4)
+
+test_suite()
+
