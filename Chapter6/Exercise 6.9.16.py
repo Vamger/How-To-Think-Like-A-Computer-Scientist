6ea6d4cc03ec9c03aa7172af478cb5d785b09aca
Vamger <mirza.adamu@hotmail.com>
2020-10-29 23:52:28 +0100

    Added changes

---
+++ Chapter6/Exercise 6.9.16.py
@@ -0,0 +1,45 @@
+# Write a function is_factor(f, n) that passes these tests:
+'''
+test(is_factor(3, 12))
+test(not is_factor(5, 12))
+test(is_factor(7, 14))
+test(not is_factor(7, 15))
+test(is_factor(1, 15))
+test(is_factor(15, 15))
+test(not is_factor(25, 15))
+'''
+
+# An important role of unit tests is that they can also act as unambiguous “specifications”
+# of what is expected. These test cases answer the question Do we treat 1 and 15 as factors of 15?
+
+import sys
+
+
+def is_factor(f,n):
+    if n % f == 0:
+        return True
+    else:
+        return None
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def test_suite():
+    """ Run the suite of tests for code in this module (this file).
+    """
+    test(is_factor(3, 12))
+    test(not is_factor(5, 12))
+    test(is_factor(7, 14))
+    test(not is_factor(7, 15))
+    test(is_factor(1, 15))
+    test(is_factor(15, 15))
+    test(not is_factor(25, 15))
+
+test_suite()
