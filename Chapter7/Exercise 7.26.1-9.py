57eb93eb8710c8d058baff93db3291c50a57fc2b
Vamger <mirza.adamu@hotmail.com>
2020-11-03 22:47:24 +0100

    Added changes

--- Chapter7/Exercise 7.26.1-9.py
+++ Chapter7/Exercise 7.26.1-9.py
@@ -1,7 +1,7 @@
 # 1. Write a function to count how many odd numbers are in a list.
 
 def count_odd_numbers():
-    for i in range(1, 10):
+    for i in range(1, 100):
         if i % 2 == 1:
             continue
         print(i)
