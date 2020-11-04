899b28b7a3042676f9c4227c052355f1301ea681
Vamger <mirza.adamu@hotmail.com>
2020-11-04 15:17:15 +0100

    Added changes

--- Chapter7/Exercise 7.26.1-9.py
+++ Chapter7/Exercise 7.26.1-9.py
@@ -1,14 +1,56 @@
 # 1. Write a function to count how many odd numbers are in a list.
 
+list = [1, 40, 13, 50, 21, 24, 25, 39, 89, 76, 21, 12]
+
 def count_odd_numbers():
-    list = [1, 40, 13, 50, 21, 24, 25, 39, 89, 76, 21, 12]
-    odd_number = 0
+    odd_number_count = 0
     for i in list:
         if i % 2 == 1:
-            odd_number += 1
-    print("There are", odd_number, "numbers in the list which are odd")
-    print("done")
-
-count_odd_numbers()
+            odd_number_count += 1
+    print("There are", odd_number_count, "numbers in the list which are odd")
 
 
+# 2. Sum up all the even numbers in a list.
+
+
+def sum_even_numbers():
+    start_value = 0
+    for i in list:
+        if i % 2 == 0:
+            start_value = start_value + i
+    print(start_value)
+    print(40+50+24+76+12)
+
+# 3. Sum upp all the negative numbers in a list
+
+list1 = [-3, 10, -45, 90, -430, 100, 12, 43]
+
+def sum_negative_numbers():
+    start_value = 0
+    for i in list1:
+        if i < 0:
+            start_value = start_value + i
+    print(start_value)
+
+# 4. Count how many words in a list have length 5.
+
+list2 = ["Apple", "Pear", "Computer", "Book", "Cards", "Ball", "Parrot", "Follow", "Balls"]
+
+def count_words():
+    w = 0
+    for i in list2:
+        if len(i) == 5:
+            w = w + 1
+    print("There are", w, "word(s) in the list which have 5 letters")
+
+
+
+
+
+# count_odd_numbers()
+# sum_even_numbers()
+# count_words()
+
+
+# 3. Sum all the elements in a list up to but not including the first even number.
+#    (Write your unit tests. What if there is no even number?)
