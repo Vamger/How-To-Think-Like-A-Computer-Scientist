5f0d0e2cd90f44f727fde704ebfb5097e5b8f4a1
Vamger <mirza.adamu@hotmail.com>
2020-11-04 13:35:57 +0100

    Added changes

--- Chapter7/Exercise 7.26.1-9.py
+++ Chapter7/Exercise 7.26.1-9.py
@@ -1,10 +1,12 @@
 # 1. Write a function to count how many odd numbers are in a list.
 
 def count_odd_numbers():
-    for i in range(1, 100):
+    list = [1, 40, 13, 50, 21, 24, 25, 39, 89, 76, 21, 12]
+    odd_number = 0
+    for i in list:
         if i % 2 == 1:
-            continue
-        print(i)
+            odd_number += 1
+    print("There are", odd_number, "numbers in the list which are odd")
     print("done")
 
 count_odd_numbers()
