ed51c215244e2ce44a4ef28b2324b70ac3ec3830
Vamger <mirza.adamu@hotmail.com>
2020-11-05 09:55:00 +0100

    Added changes

--- Chapter7/Exercise 7.26.1-9.py
+++ Chapter7/Exercise 7.26.1-9.py
@@ -63,11 +63,6 @@ def sum_list_not_first_number():
         start_value = start_value + i
     print(start_value)
 
-# count_odd_numbers()
-# sum_even_numbers()
-# sum_negative_numbers()
-# count_words()
-# sum_list_not_first_number()
 
 # 6. Count how many words occur in a list up to and including
 # the first occurrence of the word “sam”.
@@ -77,5 +72,54 @@ list4 = ["adam", "frodo", "samuel", "gandalf", "sam",
          "aragorn", "legolas", "gimli", "monster"]
 
 def count_words_w_sam():
+    start_value = 0
     for i in list4:
+        if i == "sam":
+            break
+        start_value = start_value + 1
+    print("There are", start_value + 1, "words in the list once reaching and including the word sam")
 
+
+# count_odd_numbers()
+# sum_even_numbers()
+# sum_negative_numbers()
+# count_words()
+# sum_list_not_first_number()
+# count_words_w_sam()
+
+# 7. Add a print function to Newton’s sqrt function that prints out better each time it is calculated.
+# Call your modified function with 25 as an argument and record the results.
+
+
+# Newtons sqare root function --> f(x) = ( x + 4/x)/2 = y
+# Make a continous statement, i.e. f(x) = ( y + 4/y)/2 = z
+#                                    f(x) = (z + 4/z)/2 = q
+# etc
+
+def newton_sqrt(a):
+    f1 = (a + (4/a))/2
+    while f1 <= 25:
+        f2 = (f1 + (4/f1))/2
+        print(f2)
+        if abs(f1-f2) < 0.0001:
+            return f2
+        f1 = f2
+
+newton_sqrt(25)
+
+'''
+
+def sqrt(a):
+    """Modify function square root. Printing out "better" result when
+    processing each time in loop"""
+    approx = a/2.0
+    while True:
+        better = (approx + a/approx)/2.0
+        print (better)
+        if abs(approx - better) < 0.0001:
+            return better
+        approx = better
+
+sqrt(25)
+
+'''
