7e86a00b721d2f39d68310879a0b7dd8cb3f7c22
Vamger <mirza.adamu@hotmail.com>
2020-11-06 09:46:06 +0100

    Added changes

--- Chapter7/Exercise 7.26.9.py
+++ Chapter7/Exercise 7.26.9.py
@@ -1,4 +1,4 @@
-# Write a function, is_prime, which takes a single integer argument
+# 10. Write a function, is_prime, which takes a single integer argument
 # and returns True when the argument is a prime number and False otherwise.
 # Add tests for cases like this
 
@@ -9,6 +9,18 @@ test(is_prime(19911121))
 '''
 
 import sys
+
+
+def is_prime(x):
+    if x > 1:
+        for i in range(2, x):
+            if (x % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
 
 def test(did_pass):
     """  Print the result of a test.  """
@@ -23,12 +35,9 @@ def test(did_pass):
 def test_suite():
     """ Run the suite of tests for code in this module (this file).
     """
-    test(is_multiple(12, 3))
-    test(is_multiple(12, 4))
-    test(not is_multiple(12, 5))
-    test(is_multiple(12, 6))
-    test(not is_multiple(12, 7))
+    test(is_prime(11))
+    test(not is_prime(35))
+    test(is_prime(19911121))
 
 
 test_suite()
-
